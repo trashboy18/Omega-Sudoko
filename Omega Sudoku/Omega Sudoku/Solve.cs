4b994933aae8f5179f90c91f6f35230ffb9e0222
Itay <itay7619@gmail.com>
2025-01-16 17:32:20 +0200

    built the project's structure.

---
+++ Omega Sudoku/Omega Sudoku/Solve.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omega_Sudoku
+{
+    internal class Solve
+    {
+    }
+}
