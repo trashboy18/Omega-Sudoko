8deeef3f6a082b911d3cd5f1d6205c797cbece55
Itay <itay7619@gmail.com>
2025-01-20 11:01:57 +0200

    added basic backtracking logic

--- Omega Sudoku/Omega Sudoku/Input.cs
+++ Omega Sudoku/Omega Sudoku/Input.cs
@@ -14,31 +14,14 @@ namespace Omega_Sudoku
         {
             while (true)
             {
-                try
+                Console.WriteLine("\nEnter an 81-digit Sudoku puzzle (0 for empty cells).");
+                Console.WriteLine("Or type 'exit' to quit:");
+                string input = Console.ReadLine().Trim().ToLower();
+                if (input == "exit")
                 {
-                    Console.WriteLine("\nEnter an 81-digit Sudoku puzzle (0 for empty cells).");
-                    Console.WriteLine("Or type 'exit' to quit:");
-                    string input = Console.ReadLine().Trim().ToLower();
-                    
-
-                    if (input == "exit")
-                    {
-                        break; // End the program
-                    }
-                    Helpers.CheckStringValidity(input);
-
-                    int[,] board = Conversions.StringToBoard(input);
-                    Helpers.PrintFinalBoard(board);
+                    break; // End the program
                 }
-                catch (SudokuException e)
-                {
-                    Console.WriteLine("An error occurred: " + e.Message);
-
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Error: couldn't find reason.");
-                }
+                Helpers.SolveProccess(input);
             }
         }
     }
--- Omega Sudoku/Omega Sudoku/Solve.cs
+++ Omega Sudoku/Omega Sudoku/Solve.cs
@@ -8,5 +8,38 @@ namespace Omega_Sudoku
 {
     internal class Solve
     {
+        static int N = 9;
+
+        //solving the sudoku board using pure backtracking.
+        public static bool SolveSudoku(int[,] board, int row,
+                                int col)
+        {
+            //finished backtracking.
+            if (row == N - 1 && col == N)
+                return true;
+           //go to next row.
+            if (col == N)
+            {
+                row++;
+                col = 0;
+            }
+            //check if a cell is empty.
+            if (board[row, col] != 0)
+                return SolveSudoku(board, row, col + 1);
+
+            for (int num = 1; num <=N; num++)
+            {
+                if (Helpers.IsSafe(board, row, col, num))
+                {
+                    board[row, col] = num;
+                    // Checking for next possibility with next col.
+                    if (SolveSudoku(board, row, col + 1))
+                        return true;
+                }
+                //false assumption, empty the cell.
+                board[row, col] = 0;
+            }
+            return false;
+        }
     }
 }
