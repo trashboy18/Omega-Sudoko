08f04c5ce84c91f9f093d1b7ba02705f07872d74
Itay <itay7619@gmail.com>
2025-01-20 12:58:01 +0200

    added runtime timer

--- Omega Sudoku/Omega Sudoku/Input.cs
+++ Omega Sudoku/Omega Sudoku/Input.cs
@@ -1,6 +1,7 @@
 ﻿using Omega_Sudoku.Exceptions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,11 @@ namespace Omega_Sudoku
     {
         public static void Main(string[] args)
         {
+
             while (true)
             {
+                
+
                 Console.WriteLine("\nEnter an 81-digit Sudoku puzzle (0 for empty cells).");
                 Console.WriteLine("Or type 'exit' to quit:");
                 string input = Console.ReadLine().Trim().ToLower();
@@ -21,7 +25,12 @@ namespace Omega_Sudoku
                 {
                     break; // End the program
                 }
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
                 Helpers.SolveProccess(input);
+                sw.Stop();
+                Console.WriteLine($"Sudoku solved in {sw.ElapsedMilliseconds} ms");
+
             }
         }
     }
