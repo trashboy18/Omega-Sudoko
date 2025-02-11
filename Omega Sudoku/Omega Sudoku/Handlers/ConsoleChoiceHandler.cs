00d55ed43e9ba7ab12bbaf93be15a606fbc5f8d4
Itay <itay7619@gmail.com>
2025-02-11 18:12:41 +0200

    created a folder for handlers, making the code more SOLID.

---
+++ Omega Sudoku/Omega Sudoku/Handlers/ConsoleChoiceHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omega_Sudoku.Handlers
+{
+    internal class ConsoleChoiceHandler
+    {
+        public static void HandleConsole()
+        {
+            Console.WriteLine("Enter a Sudoku puzzle string (e.g., 81 characters for a 9x9 board):");
+            string puzzleString = Console.ReadLine().Trim();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            BasicHelpers.SolveProcess(puzzleString);
+            sw.Stop();
+            Console.WriteLine($"Sudoku solved in {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
