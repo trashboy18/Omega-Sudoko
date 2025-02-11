446d640b5f266b2ce8e957eb3f6895c5a05a4562
Itay <itay7619@gmail.com>
2025-02-12 01:32:02 +0200

    bugfix- fixed tests.

--- Omega Sudoku/Omega Sudoku/Tests/ErrorTests.cs
+++ Omega Sudoku/Omega Sudoku/Tests/ErrorTests.cs
@@ -16,14 +16,34 @@ namespace Omega_Sudoku.Tests
         public void TestUnsolvable9x9a()
         {
             string input = "000005080000601043000000000010500000000106000300000005530000061000000004000000000";
-            Assert.IsFalse(BasicHelpers.SolveProcess(input).Item2);
+            Exception ex = null;
+            try
+            {
+                BasicHelpers.SolveProcess(input);
+            }
+            catch (Exception e)
+            {
+                ex = e;
+            }
+            //Assert
+            Assert.IsInstanceOfType(ex, typeof(SudokuException));
         }
 
         [TestMethod]
         public void Test2Unsolvable9x9b()
         {
-            string input = "000030000060000400007050800000406000000900000050010300400000020000300000000000000";
-            Assert.IsFalse(BasicHelpers.SolveProcess(input).Item2);
+            string input = "704000002000801000300000000506001002000400000000000900003700000900005000800000060";
+            Exception ex = null;
+            try
+            {
+                BasicHelpers.SolveProcess(input);
+            }
+            catch (Exception e)
+            {
+                ex = e;
+            }
+            //Assert
+            Assert.IsInstanceOfType(ex, typeof(SudokuException));
         }
 
         [TestMethod]
@@ -31,7 +51,17 @@ namespace Omega_Sudoku.Tests
         {
             string input = ";0?0=>010690000000710000500:?0;4000000<0400070=005<3000800000000500@000:?80>10004<30>?8;00=20000>?8;270060000000000000900000000?0000?00000>0=000?3:0000>0026000000;>61029@0<00000100<0@00:40000800500:0?;>012600800?0;0000090<0@0;07000005<00?8:00003050:4080709";
             //Assert
-            Assert.IsFalse(BasicHelpers.SolveProcess(input).Item2);
+            Exception ex = null;
+            try
+            {
+                BasicHelpers.SolveProcess(input);
+            }
+            catch (Exception e)
+            {
+                ex = e;
+            }
+            //Assert
+            Assert.IsInstanceOfType(ex, typeof(SudokuException));
         }
 
         // Tests for different Exceptions
@@ -52,7 +82,7 @@ namespace Omega_Sudoku.Tests
                 ex = e;
             }
             //Assert
-            Assert.IsInstanceOfType(ex, typeof(InvalidBoard));
+            Assert.IsInstanceOfType(ex, typeof(SudokuException));
         }
 
         [TestMethod]
